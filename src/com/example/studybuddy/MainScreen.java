0588a2bb8099c0a75f979452b83bbc2feffd8522
jslee7 <jslee7@hawaii.edu>
2016-03-14 21:02:30 -1000

    init

---
+++ src/com/example/studybuddy/MainScreen.java
@@ -0,0 +1,135 @@
+package com.example.studybuddy;
+
+import android.app.Activity;
+
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.os.Build;
+
+import android.app.*;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.view.*;
+import android.widget.ArrayAdapter;
+import android.view.View.OnTouchListener;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.ImageButton;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.io.FilenameFilter;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
+
+import org.xml.sax.helpers.XMLReaderFactory;
+import org.xml.sax.XMLReader;
+import org.xml.sax.InputSource;
+
+
+public class MainScreen extends Activity {
+	
+	
+	ImageButton flashCard;
+	//ImageButton alarm;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_screen);
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
+		}
+		
+	}
+		
+	public void runFlashcard(View v)
+    {
+        //Toast.makeText(this, "Clicked on flashcard", Toast.LENGTH_LONG).show();
+        
+        //FlashcardMain flashcard = new FlashcardMain();
+        
+        Intent i = new Intent(this, FlashcardMain.class);
+        i.setAction(Intent.ACTION_MAIN);
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+        startActivity(i);
+        
+    } 
+
+	public void runAlarm(View v)
+    {
+        Toast.makeText(this, "Clicked on alarm", Toast.LENGTH_LONG).show();
+    } 
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_screen, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main_screen, container, false);
+			return rootView;
+		}
+	}
+}
